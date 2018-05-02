fad4c3a1fac5e51078be8a521935189f0b126401
kchen36 <kchen36@stuy.edu>
2018-05-01 20:34:24 -0400

    done

--- bambi.py
+++ bambi.py
@@ -1,7 +1,24 @@
-bambi = open("bambi.txt", "r");
-bambi = bambi.read();
+b = open("bambi.txt", "r");
+bambi = b.read();
+b.close();
 bambi = bambi.split();
-print bambi;
+for i in bambi:
+    i = i.lower();
+    
+def freq(word):
+    f = len(filter(lambda x: x if x == word else None, bambi));
+    return f;
 
-len(filter(lambda x : x if x = "the", bambi))
+def gfreq(words):
+    f = sum([freq(w) for w in words]);
+    return f;
+
+def mostfreq():
+    f = reduce(lambda x, y : x if freq(x) > freq(y) else y, bambi);
+    return f;
+
+print freq("the");
+print freq("a");
+print gfreq(["the","a"])
+print mostfreq();
 
